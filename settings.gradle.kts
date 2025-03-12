4c7a9f0fe172d36da9bc81c44d464242fdd2da73
Дмитрий Бирюков <p1xelguardian@mail.ru>
2025-03-12 23:48:46 +0300

    Подтянуть версию Gradle до актуальной

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ pluginManagement {
     plugins {
         kotlin("android") version "1.7.10"
         kotlin("kapt") version "1.7.10"
-        id("com.android.application") version "7.3.1"
+        id("com.android.application") version "8.9.0"
         id("androidx.navigation.safeargs.kotlin") version navigationVersion
     }
 }
